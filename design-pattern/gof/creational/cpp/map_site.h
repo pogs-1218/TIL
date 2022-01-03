88d40a740ce3fc67b9577f9311bcefca69558d69
pogs-1218 <hooney1230@gmail.com>
2022-01-03 19:37:15 +0900

    Change the directory name c++->cpp

--- design-pattern/gof/creational/cpp/map_site.h
+++ design-pattern/gof/creational/cpp/map_site.h
@@ -1,13 +1,11 @@
 #ifndef MAP_SITE_H_
 #define MAP_SITE_H_
 
-enum class Direction {
-  North, South, East, West
-};
+enum class Direction { North, South, East, West };
 
 class MapSite {
  public:
   virtual void Enter() = 0;
 };
 
-#endif // MAP_SITE_H_
+#endif  // MAP_SITE_H_
--- design-pattern/gof/creational/cpp/maze.cc
+++ design-pattern/gof/creational/cpp/maze.cc
@@ -2,14 +2,9 @@
 
 #include <iostream>
 
-Maze::Maze()
-{
-  std::cout << "Maze is created\n";
-}
+Maze::Maze() { std::cout << "Maze is created\n"; }
 
-void Maze::AddRoom(Room* room) {
-  rooms_[room->room_number()] = room;
-}
+void Maze::AddRoom(Room* room) { rooms_[room->room_number()] = room; }
 
 Room* Maze::room(int room_number) const {
   auto search = rooms_.find(room_number);
