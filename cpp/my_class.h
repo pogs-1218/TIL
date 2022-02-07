6fc78867a644b57f6138578e77cd5ac3e77db9b2
eddie <hooney1230@gmail.com>
2022-02-07 22:39:03 +0900

    change formatting

--- cpp/my_class.h
+++ cpp/my_class.h
@@ -4,7 +4,8 @@
 
 class Rect {
  public:
-  constexpr Rect(std::size_t width, std::size_t height) : width_{width}, height_{height} {}
+  constexpr Rect(std::size_t width, std::size_t height)
+      : width_{width}, height_{height} {}
   constexpr std::size_t GetArea() const { return width_ * height_; }
 
  private:
@@ -12,19 +13,12 @@ class Rect {
   std::size_t height_{0};
 };
 
-
 class TestClass {
  public:
-  TestClass() {
-    std::cout << "Constructor\n";    
-  }
-  ~TestClass() {
-    std::cout << "Destructor\n";
-  }
+  TestClass() { std::cout << "Constructor\n"; }
+  ~TestClass() { std::cout << "Destructor\n"; }
 
-  TestClass(const TestClass& ohs) {
-    std::cout << "Copy constructor\n";
-  }
+  TestClass(const TestClass& ohs) { std::cout << "Copy constructor\n"; }
 
   TestClass& operator=(const TestClass& ohs) {
     std::cout << "Copy assign\n";
