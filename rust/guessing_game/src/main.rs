7bf1df9a44b990a71c49e71e0ca5beae6e72c57f
eddie <hooney1230@gmail.com>
2022-03-08 00:09:22 +0900

    About error handling in Rust

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -2,6 +2,22 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io; // c++: #include <iostream>
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+        Guess { value }
+    }
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
