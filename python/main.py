54ec87f2af3f3134e1bb90447af30667a6491c38
eddie <hooney1230@gmail.com>
2022-03-18 02:09:48 +0900

    Add a little bit of python

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 #from math import *
+from multiprocessing.spawn import spawn_main
 import sys
 import math
 from enum import Enum
@@ -124,3 +125,70 @@ sys.stdout.write(f"The value of pi is approximately {math.pi:.3f}\n")
 # even if an exception is raised at some point. 
 with open("test_file", "w") as test_file:
   test_file.write("This is the test\n")
+
+
+def scope_test():
+  def do_local():
+    spam = "local spam"
+
+  def do_nonlocal():
+    nonlocal spam
+    spam = "nonlocal spam"
+  
+  def do_global():
+    global spam
+    spam = "global spam"
+
+  spam = "test spam"
+  do_local()
+  print("After local assignment:", spam)
+  do_nonlocal()
+  print("After nonlocal assignment:", spam)
+  do_global()
+  print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
+
+class MyClass:
+  """ A Simple example class"""
+  i = 12345
+
+  def __init__(self, number):
+    self.j = number
+
+  def f(self):
+    return "hello world"
+
+x = MyClass(1)
+y = MyClass(2)
+
+print("xi: ", x.i)
+print("yi: ", y.i)
+
+class Reverse:
+  def __init__(self, data):
+    self.data = data
+    self.index = len(data)
+
+  def __iter__(self):
+    return self
+  
+  def __next__(self):
+    if self.index == 0:
+      raise StopIteration
+    self.index = self.index - 1
+    return self.data[self.index]
+
+rev = Reverse("spam")
+for char in rev:
+  print(char)
+
+def test_generator():
+  yield 1
+  yield 2
+  yield 3
+
+for i in test_generator():
+  print(i)
---
+++ python/stdlib/main.py
@@ -0,0 +1,11 @@
+import os
+import shutil # file/directory
+import sys
+import argparse
+
+# get current working directory
+print(os.getcwd())
+
+print(sys.argv)
+
+parser = argparse.ArgumentParser
